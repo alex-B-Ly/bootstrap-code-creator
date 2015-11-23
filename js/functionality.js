d08691cf4322bab31627b083611e9a850e574324
Alex Ly <alexly@Alexs-MBP.home>
2015-11-23 16:37:46 -0500

    added row and col add, need to add more functionality and string manipulation

--- js/functionality.js
+++ js/functionality.js
@@ -12,8 +12,31 @@ $(document).ready(function() {
   }
 
   // Pre test
+  function rowCreate(){
+    var rowButton = $('#create-row-button');
+    var rowFluidCheck = document.getElementById('row-fluid-check');
+    var codeField = document.getElementById('code');
+
+    rowButton.on('click', function() {
+      if(rowFluidCheck.checked){
+        codeField.textContent += '<div class="row-fluid"></div>\n';
+      }else{
+        codeField.textContent += '<div class="row"></div>\n';
+      }
+    });
+  }
+
+  function divCreate(){
+    var divButton = $('#create-div-button');
+    var codeField = document.getElementById('code');
+
+    divButton.on('click', function(){
+      codeField.textContent += '<div class="col"></div>\n\n'
+    });
+  }
 
   // FUNCTIONS CALLED
   menuSlide();
-
+  rowCreate();
+  divCreate();
 });
