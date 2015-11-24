eb17c69c8a96841487531de83c8f5df1cbbe9a91
Alex Ly <alexly@Alexs-MBP.home>
2015-11-23 21:34:26 -0500

    checked row if/else done

--- js/functionality.js
+++ js/functionality.js
@@ -16,10 +16,20 @@ $(document).ready(function() {
     var rowButton = $('#create-row-button');
     var rowFluidCheck = document.getElementById('row-fluid-check');
     var codeField = document.getElementById('code');
+    var idAssign = document.getElementById('id-assign');
+    var classAssign = document.getElementById('class-assign');
 
     rowButton.on('click', function() {
       if(rowFluidCheck.checked){
-        codeField.textContent += '<div class="row-fluid">\n\n';
+        if(idAssign.value && classAssign.value){
+          codeField.textContent += '<div id="'+ idAssign.value +'" class="row-fluid '+ classAssign.value + '">\n\n';  
+        }else if(idAssign.value){
+          codeField.textContent += '<div id="'+ idAssign.value +'" class="row-fluid">\n\n';  
+        }else if(classAssign.value){
+          codeField.textContent += '<div class="row-fluid '+ classAssign.value +'">\n\n';
+        }else{
+          codeField.textContent += '<div class="row-fluid">\n\n';
+        }
       }else{
         codeField.textContent += '<div class="row">\n\n';
       }
